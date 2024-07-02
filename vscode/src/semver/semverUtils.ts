030961c0f10bcd2c3646296fb5931d0d9bff8129
Kadir Kaan Göç <114440976+kadirkaang@users.noreply.github.com>
2024-07-02 15:48:21 +0300

    feat: Add valid and gt functions to semverUtils.ts (#2)

--- vscode/src/semver/semverUtils.ts
+++ vscode/src/semver/semverUtils.ts
@@ -1,4 +1,4 @@
-import { maxSatisfying, satisfies } from "semver";
+import { maxSatisfying, satisfies, valid, gt } from "semver";
 
 
 export function checkVersion(version: string = "0.0.0", versions: string[]): [boolean, string | null] {
@@ -7,5 +7,13 @@ export function checkVersion(version: string = "0.0.0", versions: string[]): [bo
   if (prefix > 47 && prefix < 58)
     v = "^" + v;
   const max = versions[0];
-  return [satisfies(max, v), maxSatisfying(versions, version)];
+  if (maxSatisfying(versions, v) === null) {
+    if (valid(v) === null) {
+      return [false, null];
+    }
+    if (gt(v, max)) {
+      return [true, v]
+    }
+  }
+  return [satisfies(max, v), maxSatisfying(versions, v)];
 }
