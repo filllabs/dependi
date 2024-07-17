3c8d953b678189804854ff6ebe96368469eae62f
Enzo Innocenzi <enzo@innocenzi.dev>
2024-07-17 18:49:24 +0200

    fix: disable checking of `link:` in npm

--- vscode/src/core/parsers/NpmParser.ts
+++ vscode/src/core/parsers/NpmParser.ts
@@ -1,7 +1,18 @@
+import Item from "../Item";
 import { JsonParser } from "./JsonParser";
 
 export class NpmParser extends JsonParser {
 	constructor() {
 		super('dependencies', 'devDependencies')
 	}
+
+  addDependency(item: Item) {
+    if (item.value?.startsWith('link:')) {
+      return
+    }
+    
+    item.createRange();
+    item.createDecoRange();
+    this.state.items.push(item);
+  }
 }
