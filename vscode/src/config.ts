4b971531a1dfd6e808d9a9b53d8cf42219491477
Seray Uzgur <serayuzgur@gmail.com>
2024-07-24 13:16:58 +0300

    Add patch update decoration (#56)
    * Add patch update decoration
    
    * feat: Add setting for informing patch updates per language
    
    * chore: Remove console.log statement in decoration.ts

--- vscode/src/config.ts
+++ vscode/src/config.ts
@@ -9,26 +9,31 @@ export enum Configs {
   RUST_INDEX_SERVER_URL = `rust.indexServerURL`,
   RUST_IGNORE_UNSTABLES = `rust.excludeUnstableVersions`,
   RUST_IGNORE_LINE_PATTERN = `rust.ignoreLinePattern`,
+  RUST_INFORM_PATCH_UPDATES = `rust.informPatchUpdates`,
 
   NPM_ENABLED = `npm.enabled`,
   NPM_INDEX_SERVER_URL = `npm.indexServerURL`,
   NPM_IGNORE_UNSTABLES = `npm.excludeUnstableVersions`,
   NPM_IGNORE_LINE_PATTERN = `npm.ignoreLinePattern`,
+  NPM_INFORM_PATCH_UPDATES = `npm.informPatchUpdates`,
 
   PHP_ENABLED = `php.enabled`,
   PHP_INDEX_SERVER_URL = `php.indexServerURL`,
   PHP_IGNORE_UNSTABLES = `php.excludeUnstableVersions`,
   PHP_IGNORE_LINE_PATTERN = `php.ignoreLinePattern`,
+  PHP_INFORM_PATCH_UPDATES = `php.informPatchUpdates`,
 
   GO_ENABLED = `go.enabled`,
   GO_INDEX_SERVER_URL = `go.indexServerURL`,
   GO_IGNORE_UNSTABLES = `go.excludeUnstableVersions`,
   GO_IGNORE_LINE_PATTERN = `go.ignoreLinePattern`,
+  GO_INFORM_PATCH_UPDATES = `go.informPatchUpdates`,
 
   PYTHON_ENABLED = `python.enabled`,
   PYTHON_INDEX_SERVER_URL = `python.indexServerURL`,
   PYTHON_IGNORE_UNSTABLES = `python.excludeUnstableVersions`,
   PYTHON_IGNORE_LINE_PATTERN = `python.ignoreLinePattern`,
+  PYTHON_INFORM_PATCH_UPDATES = `python.informPatchUpdates`,
 
   VULS_ENABLED = `vulnerability.enabled`,
   VULS_GHSA_ENABLED = `vulnerability.ghsa.enabled`,
@@ -45,6 +50,8 @@ export enum Configs {
   ERROR_DECORATOR_CSS = `decoration.error.style`,
   INCOMPATIBLE_DECORATOR = `decoration.incompatible.template`,
   INCOMPATIBLE_DECORATOR_CSS = `decoration.incompatible.style`,
+  PATCH_UPDATE_DECORATOR = `decoration.patchUpdate.template`,
+  PATCH_UPDATE_DECORATOR_CSS = `decoration.patchUpdate.style`,
   COMPATIBLE_DECORATOR = `decoration.compatible.template`,
   COMPATIBLE_DECORATOR_CSS = `decoration.compatible.style`,
   VULNERABILITY_DECORATOR = `decoration.vulnerability.template`,
@@ -68,31 +75,37 @@ export const Settings = {
     enabled: true,
     index: "",
     ignoreUnstable: false,
-    ignoreLinePattern: ""
+    ignoreLinePattern: "",
+    informPatchUpdates: false
+
   },
   npm: {
     enabled: true,
     index: "",
     ignoreUnstable: false,
-    ignoreLinePattern: ""
+    ignoreLinePattern: "",
+    informPatchUpdates: false
   },
   php: {
     enabled: true,
     index: "",
     ignoreUnstable: false,
-    ignoreLinePattern: ""
+    ignoreLinePattern: "",
+    informPatchUpdates: false
   },
   go: {
     enabled: true,
     index: "",
     ignoreUnstable: false,
-    ignoreLinePattern: ""
+    ignoreLinePattern: "",
+    informPatchUpdates: false
   },
   python: {
     enabled: true,
     index: "",
     ignoreUnstable: false,
-    ignoreLinePattern: ""
+    ignoreLinePattern: "",
+    informPatchUpdates: false
   },
   vulnerability: {
     enabled: false,
@@ -117,6 +130,10 @@ export const Settings = {
       template: "",
       css: {}
     },
+    patchUpdate: {
+      template: "",
+      css: {}
+    },
     compatible: {
       template: "",
       css: {}
@@ -134,26 +151,31 @@ export const Settings = {
     this.rust.index = config.get<string>(Configs.RUST_INDEX_SERVER_URL) || "https://index.crates.io";
     this.rust.ignoreUnstable = config.get<boolean>(Configs.RUST_IGNORE_UNSTABLES) ?? true;
     this.rust.ignoreLinePattern = config.get<string>(Configs.RUST_IGNORE_LINE_PATTERN) || "";
+    this.rust.informPatchUpdates = config.get<boolean>(Configs.RUST_INFORM_PATCH_UPDATES) ?? false;
 
     this.npm.enabled = config.get<boolean>(Configs.NPM_ENABLED) ?? true;
     this.npm.index = config.get<string>(Configs.NPM_INDEX_SERVER_URL) || "https://registry.npmjs.org";
     this.npm.ignoreUnstable = config.get<boolean>(Configs.NPM_IGNORE_UNSTABLES) ?? true;
     this.npm.ignoreLinePattern = config.get<string>(Configs.NPM_IGNORE_LINE_PATTERN) || "";
+    this.npm.informPatchUpdates = config.get<boolean>(Configs.NPM_INFORM_PATCH_UPDATES) ?? false;
 
     this.php.enabled = config.get<boolean>(Configs.PHP_ENABLED) ?? true;
     this.php.index = config.get<string>(Configs.PHP_INDEX_SERVER_URL) || "https://repo.packagist.org";
     this.php.ignoreUnstable = config.get<boolean>(Configs.PHP_IGNORE_UNSTABLES) ?? true;
     this.php.ignoreLinePattern = config.get<string>(Configs.PHP_IGNORE_LINE_PATTERN) || "";
+    this.php.informPatchUpdates = config.get<boolean>(Configs.PHP_INFORM_PATCH_UPDATES) ?? false;
 
     this.go.enabled = config.get<boolean>(Configs.GO_ENABLED) ?? true;
     this.go.index = config.get<string>(Configs.GO_INDEX_SERVER_URL) || "https://proxy.golang.org";
     this.go.ignoreUnstable = config.get<boolean>(Configs.GO_IGNORE_UNSTABLES) ?? true;
     this.go.ignoreLinePattern = config.get<string>(Configs.GO_IGNORE_LINE_PATTERN) || "";
+    this.go.informPatchUpdates = config.get<boolean>(Configs.GO_INFORM_PATCH_UPDATES) ?? false;
 
     this.python.enabled = config.get<boolean>(Configs.PYTHON_ENABLED) ?? true;
     this.python.index = config.get<string>(Configs.PYTHON_INDEX_SERVER_URL) || "https://pypi.org/pypi";
     this.python.ignoreUnstable = config.get<boolean>(Configs.PYTHON_IGNORE_UNSTABLES) ?? true;
     this.python.ignoreLinePattern = config.get<string>(Configs.PYTHON_IGNORE_LINE_PATTERN) || "";
+    this.python.informPatchUpdates = config.get<boolean>(Configs.PYTHON_INFORM_PATCH_UPDATES) ?? false;
 
     this.vulnerability.enabled = config.get<boolean>(Configs.VULS_ENABLED) ?? true;
     this.vulnerability.ghsa = config.get<boolean>(Configs.VULS_GHSA_ENABLED) ?? false;
@@ -171,6 +193,8 @@ export const Settings = {
     this.decorator.error.css = config.get<DecorationInstanceRenderOptions>(Configs.ERROR_DECORATOR_CSS) || {};
     this.decorator.incompatible.template = config.get<string>(Configs.INCOMPATIBLE_DECORATOR) || "❌ ${version}";
     this.decorator.incompatible.css = config.get<DecorationInstanceRenderOptions>(Configs.INCOMPATIBLE_DECORATOR_CSS) || {};
+    this.decorator.patchUpdate.template = config.get<string>(Configs.PATCH_UPDATE_DECORATOR) || "⚠️ ${version}";
+    this.decorator.patchUpdate.css = config.get<DecorationInstanceRenderOptions>(Configs.PATCH_UPDATE_DECORATOR_CSS) || {};
     this.decorator.compatible.template = config.get<string>(Configs.COMPATIBLE_DECORATOR) || "✅";
     this.decorator.compatible.css = config.get<DecorationInstanceRenderOptions>(Configs.COMPATIBLE_DECORATOR_CSS) || {};
     this.decorator.vulnerability.template = config.get<string>(Configs.VULNERABILITY_DECORATOR) || "🚨 ${count}";
--- vscode/src/semver/semverUtils.ts
+++ vscode/src/semver/semverUtils.ts
@@ -1,7 +1,9 @@
-import { maxSatisfying, satisfies, valid, gt } from "semver";
+import { compare, gt, maxSatisfying, minVersion, satisfies, valid } from "semver";
+import { Settings } from "../config";
+import { CurrentLanguage, Language } from "../core/Language";
 
 
-export function checkVersion(version: string = "0.0.0", versions: string[]): [boolean, string | null] {
+export function checkVersion(version: string = "0.0.0", versions: string[]): [boolean, boolean, string | null] {
   let v = version;
   let prefix = v.charCodeAt(0);
   if (prefix > 47 && prefix < 58)
@@ -9,11 +11,33 @@ export function checkVersion(version: string = "0.0.0", versions: string[]): [bo
   const max = versions[0];
   if (maxSatisfying(versions, v) === null) {
     if (valid(v) === null) {
-      return [false, null];
+      return [false, false, null];
     }
-    if (gt(v, max)) {
-      return [true, v]
+    // TODO: ask this test with kaan
+    const minV = minVersion(v)?.toString() ?? '0.0.0';
+    if (gt(minV, max)) {
+      return [true, true, v];
     }
   }
-  return [satisfies(max, v), maxSatisfying(versions, v)];
+  // if check patch is true, check if the patch version is the same or higher than the current version
+  let shouldPatchBeChecked = false;
+  switch (CurrentLanguage) {
+    case Language.Rust:
+      shouldPatchBeChecked = Settings.rust.informPatchUpdates;
+      break;
+    case Language.JS:
+      shouldPatchBeChecked = Settings.npm.informPatchUpdates;
+      break;
+    case Language.PHP:
+      shouldPatchBeChecked = Settings.php.informPatchUpdates;
+      break;
+    case Language.Golang:
+      shouldPatchBeChecked = Settings.go.informPatchUpdates;
+      break;
+    case Language.Python:
+      shouldPatchBeChecked = Settings.python.informPatchUpdates;
+      break;
+  }
+  const pathUpdated = shouldPatchBeChecked ? compare(max, minVersion(v) ?? '0.0.0') === 1 : false;
+  return [satisfies(max, v), pathUpdated, maxSatisfying(versions, v)];
 }
