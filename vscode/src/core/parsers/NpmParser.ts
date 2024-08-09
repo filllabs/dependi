c232709047fb91f75f0489f10411ed5f5ced75c6
Kadir Kaan Göç <114440976+kadirkaang@users.noreply.github.com>
2024-08-09 12:44:00 +0300

    refactor: Convert npm aliases to package names in NpmParser.ts (#99)

--- vscode/src/core/parsers/NpmParser.ts
+++ vscode/src/core/parsers/NpmParser.ts
@@ -10,9 +10,21 @@ export class NpmParser extends JsonParser {
     if (item.value?.startsWith('link:')) {
       return
     }
-    
+    item = convertAliasToPackageName(item);
     item.createRange();
     item.createDecoRange();
     this.state.items.push(item);
   }
+}
+
+function convertAliasToPackageName(item: Item): Item {
+  if (item.value?.startsWith("npm:")) {
+    const atSymbolIndex = item.value.indexOf('@');
+    if (atSymbolIndex !== -1) {
+      item.key = item.value.slice(4, atSymbolIndex);
+      item.value = item.value.slice(atSymbolIndex + 1);
+      item.start = item.start + item.key.length + 5;
+    }
+  }
+  return item
 }
