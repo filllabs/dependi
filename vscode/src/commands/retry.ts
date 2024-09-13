f27469cc58c44d0f10a8a755f40068c9bfd8fd6f
Kadir Kaan Göç <114440976+kadirkaang@users.noreply.github.com>
2024-09-13 17:21:34 +0300

    fix: Clear dependency cache (#142)
    * fix: Clear dependency cache
    
    * fix: Use UnstableFilter.Exclude enum for unstableFilter.

--- vscode/src/commands/retry.ts
+++ vscode/src/commands/retry.ts
@@ -1,11 +1,14 @@
 import { commands, TextEditor, TextEditorEdit } from "vscode";
 import { Configs } from "../config";
 import listener from "../core/listeners";
+import { DependencyCache } from "../core/listeners/listener";
+import { CurrentLanguage } from "../core/Language";
 
 export const retry = commands.registerTextEditorCommand(
   Configs.RETRY,
   (editor: TextEditor, edit: TextEditorEdit, info) => {
     if (editor) {
+      DependencyCache.delete(CurrentLanguage);
       listener(editor);
     }
   },
