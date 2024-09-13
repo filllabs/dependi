06c3f55f864b25132eb60767efeb19452f5b3b2c
Kadir Kaan Göç <114440976+kadirkaang@users.noreply.github.com>
2024-09-13 16:00:37 +0300

    fix: Latest tag bug (#141)

--- vscode/src/core/listeners/listener.ts
+++ vscode/src/core/listeners/listener.ts
@@ -35,7 +35,8 @@ export abstract class Listener {
     dependencies = dependencies.map(dep => {
       let cached = cache.get<Dependency>(dep.item.key + dep.item.range.start.line);
       dep.item.value = dep.item.lockedAt ?? dep.item.value;
-      if (cached && dep.item.value !== "latest") {
+      if (cached) {
+        if (dep.item.value === "latest") return cached;
         cached.item = dep.item;
       } else {
         cache.set(dep.item.key + dep.item.range.start.line, dep);
