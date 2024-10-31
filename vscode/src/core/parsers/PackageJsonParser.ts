c38c5a993f7c032ab9a946c51b9ca7de56d5e74d
kadirkaang <kadirkaang52@gmail.com>
2024-10-31 12:22:04 +0300

    feat: Enhance NpmParser to read catalog values from pnpm-workspace.yaml

--- vscode/src/core/parsers/PackageJsonParser.ts
+++ vscode/src/core/parsers/PackageJsonParser.ts
@@ -1,7 +1,11 @@
 import { Settings } from "../../config";
+import * as vscode from "vscode";
 import Item from "../Item";
 import { JsonParser } from "./JsonParser";
 import { PackageLockJsonParser } from "./PackageLockJsonParser";
+import path from "path";
+import * as fs from "fs"
+import { clearText } from "./TomlParser";
 
 export class NpmParser extends JsonParser {
   constructor() {
@@ -14,7 +18,7 @@ export class NpmParser extends JsonParser {
       ],
       Settings.npm.lockFileEnabled,
       "package-lock.json",
-      new PackageLockJsonParser(),
+      new PackageLockJsonParser()
     );
   }
 
@@ -25,6 +29,29 @@ export class NpmParser extends JsonParser {
     item = convertAliasToPackageName(item);
     item.createRange();
     item.createDecoRange();
+    if (item.value && item.value?.startsWith("catalog:")) {
+      const catalog = item.value.replace("catalog:", "");
+      const filePath = vscode.window.activeTextEditor?.document.fileName
+      const dirName = path.dirname(filePath || "");
+      try {
+        const files = fs.readdirSync(dirName);
+        const yamlFile = files.find((file) => file === "pnpm-workspace.yaml");
+        if (yamlFile) {
+          const yamlFilePath = path.join(dirName, yamlFile);
+          const yamlContent = fs.readFileSync(yamlFilePath, "utf8");
+          const yamlLines = yamlContent.split("\n");
+          yamlLines.forEach((line) => {
+            console.log(line);
+            if (line.includes(catalog)) {
+              const catalogName = line.split(":")[1].trim();
+              item.value = clearText(catalogName);
+            }
+          });
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
     this.state.items.push(item);
   }
 }
